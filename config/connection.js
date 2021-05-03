45fd31e941dab4a8f00480fd28ac74bf6340d09a
shelly-Tan <zoeshellytzl@gmail.com>
2021-05-03 12:24:08 +0930

    update

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  process.env.JAWSDB_URL,
   {
     host: 'localhost',
     dialect: 'mysql',
